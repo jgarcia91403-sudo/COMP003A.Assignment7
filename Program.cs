c3dd06af5bea1ae9140e96e8be2d208cc4758cef
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-02-27 15:47:47 -0800

    I done the first part listing the scores and putting options to choose from.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace COMP003A.Assignment7
+﻿using System;
+using System.Collections.Generic;
+namespace COMP003A.Assignment7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> scores = new List<int>() { 85, 90, 78, 92, 88 };
+            int choice = 0;
+            while (choice != 4)
+            {
+                Console.WriteLine("\nStudent score Analyzer");
+                Console.WriteLine("1. Display all scores");
+                Console.WriteLine("2. show total");
+                Console.WriteLine("3. show Average");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice:");
+            }
         }
     }
 }
