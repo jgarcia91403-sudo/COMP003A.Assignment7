9cee03da7ecc212678341e46f2dd0eb3515dd8b9
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-02-27 16:18:19 -0800

    I finished 1 out the 4 choice that are going to be picked.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,35 @@ namespace COMP003A.Assignment7
                 Console.WriteLine("3. show Average");
                 Console.WriteLine("4. Exit");
                 Console.Write("Enter your choice:");
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. pleaes enter a number between 1 and 4.");
+                    choice = 0;
+                }
+                if (choice >= 1 && choice <= 4)
+                {
+                    if (choice == 1)
+                    {
+                        Console.WriteLine("\nScores:");
+                        foreach (int score in scores)
+                        {
+                            Console.WriteLine(score);
+                        }
+                    }
+                    else if (choice == 2)
+                    {
+                        int total = 0;
+foreach (int score in scores)
+                        {
+                            total += score;
+                        }
+
+                    }
+                }
             }
         }
     }
